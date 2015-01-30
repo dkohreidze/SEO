fc1079d2d80dc4e537250d6bf2f8bfdf3c48c922
David Kohreidze <dkohreidze@gmail.com>
2015-01-29 18:05:47 -0800

    auto_link.py

--- auto_link.py
+++ auto_link.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python
 
-#David Kohreidze
-
 import os
 import re
 
 links = {	#keywords		#URLs
-			'auto link' :	'https://github.com/dkohreidze/SEO'	,
-			'Google'	:	'https://www.google.com'	
-		}
+		'auto link' :	'https://github.com/dkohreidze/SEO',
+		'Google'	:	'https://www.google.com'
+	}
 
 for fn in os.listdir('.'):
   if os.path.isfile(fn):
     if fn.endswith(".txt"):
 	  s = open(fn).read()
-      print "Processing %s.." %fn
+	  print "Processing %s.." %fn
 	  for i in links:
 		link = links[i]		
   		s = re.sub(r'\b'+i+r'\b', '<a href="%s">%s</a>'%(link,i), s, 1)
