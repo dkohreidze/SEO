694de55f52eee109da07e78e5abf29e70148e29d
David Kohreidze <david@fountain.com>
2015-04-28 18:49:58 -0700

    removed redundant code

--- sail.py
+++ sail.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python
 # David Kohreidze
-# sail.py
 
 import os
 import re
 
 links = {	#keywords	#URLs
 		'auto link' :  'https://github.com/dkohreidze/seo-auto-internal-links',
-		'Google'    :  'https://www.google.com'
+		'Google'    :  'https://www.google.com' # case sensitive Google
 	}
 
-for fn in os.listdir('.'):
-  if os.path.isfile(fn):
-    if fn.endswith(".txt"):
-	  s = open(fn).read()
-	  print "Processing %s.." %fn
+for f in os.listdir('.'): # for every file in the current directory
+  if os.path.isfile(f): # must be a file
+    if f.endswith(".txt"): # must be a text file
+	  s = open(f).read() # read file
+	  print "Processing %s.." %f
+	  
 	  for i in links:
-		link = links[i]		
-  		s = re.sub(r'\b'+i+r'\b', '<a href="%s">%s</a>'%(link,i), s, 1)
-	  f = open(fn, 'w')
+		s = re.sub(r'\b'+i+r'\b', '<a href="%s">%s</a>'%(links[i],i), s, 1)
+	 
+	  f = open(f, 'w')
 	  f.write(s)
 	  f.close()
 print "Complete."
